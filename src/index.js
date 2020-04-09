1438f7a64fb7a43f1c3778b06dda9fa44c8db339
Achref Rouabeh <42032975+BuildDev@users.noreply.github.com>
2020-04-09 18:43:20 +0100

    install redux and create the user reducer

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 
