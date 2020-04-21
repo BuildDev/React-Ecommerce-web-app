6569ff21e3486c4fd8a712828c23d34eceaca86b
Achref Rouabeh <42032975+BuildDev@users.noreply.github.com>
2020-04-21 14:00:56 +0100

    #install redux-persist and save local storage

--- src/index.js
+++ src/index.js
@@ -5,14 +5,16 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
-import 'font-awesome/css/font-awesome.min.css';
-
+import { store, persistor } from "./redux/store";
+import "font-awesome/css/font-awesome.min.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
