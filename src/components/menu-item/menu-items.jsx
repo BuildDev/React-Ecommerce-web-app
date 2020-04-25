781f7cbcfef7183855c7633e0e73c1efc9b9ba37
Achref Rouabeh <42032975+BuildDev@users.noreply.github.com>
2020-04-25 17:29:32 +0100

    #add new HOC

--- src/components/menu-item/menu-items.jsx
+++ src/components/menu-item/menu-items.jsx
@@ -16,7 +16,7 @@ const MenuItems = ({ id, title, imageUrl, size, linkUrl, match, history }) => {
       />
       <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
-        <span className="subtitle">SHOPE NOW</span>
+        <span className="subtitle">SHOP NOW</span>
       </div>
     </div>
   );
